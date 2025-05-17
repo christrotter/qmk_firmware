39ccac6975d6ea9f5c629096dbf6b583e61cd08a
Chris Trotter <ctrotter@gmail.com>
2025-05-17 13:36:26 -0400

    removing useless rgb enable

--- keyboards/handwired/arcboard_mk19/post_rules.mk
+++ keyboards/handwired/arcboard_mk19/post_rules.mk
@@ -1,7 +1,7 @@
 
-ifeq ($(strip $(RGB_MATRIX_ENABLE)), yes)
-	OPT_DEFS += -DRGB_MATRIX_ENABLE
-endif
+# ifeq ($(strip $(RGB_MATRIX_ENABLE)), yes)
+# 	OPT_DEFS += -DRGB_MATRIX_ENABLE
+# endif
 ifeq ($(strip $(DRAGSCROLL_ENABLE)), yes)
 	OPT_DEFS += -DDRAGSCROLL_ENABLE
 endif
--- keyboards/handwired/arcpedals_mk3/post_rules.mk
+++
@@ -1,4 +0,0 @@
-
-ifeq ($(strip $(RGB_MATRIX_ENABLE)), yes)
-	OPT_DEFS += -DRGB_MATRIX_ENABLE
-endif
