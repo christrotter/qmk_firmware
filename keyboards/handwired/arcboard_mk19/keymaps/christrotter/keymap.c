60baf18fe8b253ecea49119d93f4c2525de52918
Chris Trotter <ctrotter@gmail.com>
2025-05-17 13:47:16 -0400

    adding if statement as per elpekinin

--- keyboards/handwired/arcboard_mk19/keymaps/christrotter/keymap.c
+++ keyboards/handwired/arcboard_mk19/keymaps/christrotter/keymap.c
@@ -264,6 +264,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     //     set_oneshot_mods(MOD_LGUI);
     //     return false;
     // }
+    if (!record->event.pressed) {return true;}
+
     // now we check for specific keycodes...
     #if defined(CUSTOM_KEYCODES)
         switch (keycode) {
